52ef37f8813268918d7f96c0926fd2221cc720f6
Jeremy Wright <wrightjmf@gmail.com>
2021-07-30 15:54:45 -0400

    Added tests for the group buttons on the operations dialog

--- tests/actions_dialog/test_main_dialog.gd
+++ tests/actions_dialog/test_main_dialog.gd
@@ -17,12 +17,66 @@ func test_workplane_button():
 	wp_btn.pressed = true
 	wp_btn.emit_signal("toggled", wp_btn)
 
-	# Give the file time to be written
-#	yield(yield_to(wp_btn, "toggled", 5), YIELD)
-
 	# Check to make sure the ActionOptionButton shows the correct default selection
 	var action_btn = popup.get_node("VBoxContainer/ActionOptionButton")
 	assert_eq(action_btn.get_item_text(action_btn.get_selected_id()), "New Component")
 
-	wp_btn.free()
-	popup.free()
+
+"""
+Tests as if the user clicked the 3D button.
+"""
+func test_threed_button():
+	# Get a reference to the whole interface and make sure we got it
+	var gui = partial_double("res://GUI.tscn").instance()
+	assert_not_null(gui)
+	var popup = gui.get_node("ActionPopupPanel")
+	assert_not_null(popup)
+
+	# Simulate a click of the workplane button
+	var threed_btn = popup.get_node("VBoxContainer/ActionGroupsVBoxContainer/HBoxContainer/ThreeDButton")
+	threed_btn.pressed = true
+	threed_btn.emit_signal("toggled", threed_btn)
+
+	# Check to make sure the ActionOptionButton shows the correct default selection
+	var action_btn = popup.get_node("VBoxContainer/ActionOptionButton")
+	assert_eq(action_btn.get_item_text(action_btn.get_selected_id()), "Blind Cut (cutBlind)")
+
+
+"""
+Tests as if the user clicked the Sketch button.
+"""
+func test_sketch_button():
+	# Get a reference to the whole interface and make sure we got it
+	var gui = partial_double("res://GUI.tscn").instance()
+	assert_not_null(gui)
+	var popup = gui.get_node("ActionPopupPanel")
+	assert_not_null(popup)
+
+	# Simulate a click of the workplane button
+	var threed_btn = popup.get_node("VBoxContainer/ActionGroupsVBoxContainer/HBoxContainer/SketchButton")
+	threed_btn.pressed = true
+	threed_btn.emit_signal("toggled", threed_btn)
+
+	# Check to make sure the ActionOptionButton shows the correct default selection
+	var action_btn = popup.get_node("VBoxContainer/ActionOptionButton")
+	assert_eq(action_btn.get_item_text(action_btn.get_selected_id()), "Circle (circle)")
+
+
+"""
+Tests as if the user clicked the Selectors button.
+"""
+func test_selector_button():
+	# Get a reference to the whole interface and make sure we got it
+	var gui = partial_double("res://GUI.tscn").instance()
+	assert_not_null(gui)
+	var popup = gui.get_node("ActionPopupPanel")
+	assert_not_null(popup)
+
+	# Simulate a click of the workplane button
+	var threed_btn = popup.get_node("VBoxContainer/ActionGroupsVBoxContainer/HBoxContainer/SelectorButton")
+	threed_btn.pressed = true
+	threed_btn.emit_signal("toggled", threed_btn)
+
+	# Check to make sure the ActionOptionButton shows the correct default selection
+	var action_btn = popup.get_node("VBoxContainer/ActionOptionButton")
+	assert_eq(action_btn.get_item_text(action_btn.get_selected_id()), "selectors")
