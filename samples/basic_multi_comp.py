b51f8f40fcec8ac4b8e8dea6e61c78b2ff545a8b
Jeremy Wright <wrightjmf@gmail.com>
2021-11-08 10:02:04 -0500

    Fixed multi-component file

--- samples/basic_multi_comp.py
+++ samples/basic_multi_comp.py
@@ -1,11 +1,11 @@
-# Semblage v0.2.0-alpha
+# Semblage v0.4.0-alpha
 import cadquery as cq
 # start_params
 # end_params
 change_me=cq
-change_me2=cq
 change_me=change_me.Workplane().tag("change_me")
 change_me=change_me.box(10,10,10)
+change_me2=cq
 change_me2=change_me2.Workplane().tag("change_me2")
 change_me2=change_me2.circle(1.0)
 change_me2=change_me2.extrude(15)
