74dfd77eca43389adcdcd48027603cbc331a05f7
Jeremy Wright <wrightjmf@gmail.com>
2022-01-24 07:17:13 -0500

    Made the min-max face synthesizer more efficient and prepared for indexing

--- Synthesis.gd
+++ Synthesis.gd
@@ -66,32 +66,95 @@ static func synthesize(faces):
 Attempts to synthesize a maximum/minimum selelector string.
 """
 static func synthesize_max_min_face(faces):
-	var selector_str = null
+	#var selector_str = null
 
 	var selected_origin = faces["selected_origins"][0]
 	var selected_normal = faces["selected_normals"][0]
 
+	var selector_str = '.faces("{modifier}{axis}{index}")'
+	var axis_index = -1
+	var axis_str = ""
+	var modifier_str = ""
+	var index_str = ""
+
 	# Determine if the face's normal is aligned with any axis
 	if is_parallel_to_x(selected_normal):
-		# See if the given face is the maximum in the X direction
-		if is_maximum_in_axis(faces, selected_origin, selected_normal, 0):
-			selector_str = '.faces(">X")'
-		elif is_minimum_in_axis(faces, selected_origin, selected_normal, 0):
-			selector_str = '.faces("<X")'
+		axis_str = "X"
+		axis_index = 0
 	elif is_parallel_to_y(selected_normal):
-		# See if the given face is the maximum in the X direction
-		if is_maximum_in_axis(faces, selected_origin, selected_normal, 1):
-			selector_str = '.faces(">Y")'
-		elif is_minimum_in_axis(faces, selected_origin, selected_normal, 1):
-			selector_str = '.faces("<Y")'
+		axis_str = "Y"
+		axis_index = 1
 	elif is_parallel_to_z(selected_normal):
-		# See if the given face is the maximum in the X direction
-		if is_maximum_in_axis(faces, selected_origin, selected_normal, 2):
-			selector_str = '.faces(">Z")'
-		elif is_minimum_in_axis(faces, selected_origin, selected_normal, 2):
-			selector_str = '.faces("<Z")'
+		axis_str = "Z"
+		axis_index = 2
+
+	# Check to see if the selected face is aligned with an axis
+	if axis_index > -1:
+		# See if the face is either the minimum or maximum in this axis
+		var min_max = find_min_max_in_axis(faces, selected_origin, selected_normal, axis_index)
+		# Format the min/max filter
+		if min_max[0] == true:
+			modifier_str = "<"
+		elif min_max[1] == true:
+			modifier_str = ">"
+
+		# Find out if we have an indexed max or min
+		if min_max[2] != -1:
+			index_str = str(min_max[2])
+
+		# If we did not find the axis and modifier, the selector string should be null
+		if axis_str != "" and modifier_str != "":
+			selector_str = selector_str.format({"axis": axis_str, "modifier": modifier_str, "index": index_str})
+		else:
+			selector_str = null
+	else:
+		# Let the user know a match was not found
+		selector_str = null
 
 	return selector_str
+
+
+"""
+Determines whether or not a face is the minimum or maximum along an axis, and
+what the index is, if any.
+"""
+static func find_min_max_in_axis(faces, selected_origin, selected_normal, axis_index):
+	var is_max = null
+	var is_min = null
+
+	var i = 0
+	for face in faces["other_faces"]:
+		# Check if the face normals are aligned
+		if is_parallel(selected_normal, faces["other_normals"][i]):
+			# Check to see if the face is the maximum along the axis
+			if selected_origin[axis_index] > faces["other_origins"][i][axis_index]:
+				# Keep from overriding other faces that were already more maximal
+				if is_max == null:
+					is_max = true
+			else:
+				is_max = false
+
+			# Check to see if the face is the minimum along the axis
+			if selected_origin[axis_index] < faces["other_origins"][i][axis_index]:
+				# Keep from overriding other faces that were already more minimal
+				if is_min == null:
+					is_min = true
+			else:
+				is_min = false
+
+		i += 1
+
+	# If this is still null, then the face is not maximum
+	if is_max == null:
+		is_max = false
+	# If this is still null, then the face is not minimum
+	if is_min == null:
+		is_min = false
+
+	# TODO: Determine if the face is indexed min or max here
+	var index = -1
+
+	return [is_min, is_max, index]
 
 
 """
