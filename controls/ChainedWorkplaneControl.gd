8ff6345499a34b86e306dd7ec7f2c7eea2782684
Jeremy Wright <wrightjmf@gmail.com>
2022-03-12 10:56:19 -0500

    Added an offset control to the chained workplane operation control

--- controls/ChainedWorkplaneControl.gd
+++ controls/ChainedWorkplaneControl.gd
@@ -4,12 +4,13 @@ class_name ChainedWorkplaneControl
 
 var prev_template = null
 
-var template = ".workplane(invert={invert},centerOption=\"{center_option}\")" #.tag(\"{comp_name}\")"
+var template = ".workplane(offset={offset},invert={invert},centerOption=\"{center_option}\")" #.tag(\"{comp_name}\")"
 
 const center_option_list = ["CenterOfBoundBox", "CenterOfMass", "ProjectedOrigin"]
 
 const wp_cen_edit_rgx = "(?<=centerOption\\=\")(.*?)(?=\"\\))"
 const invert_edit_rgx = "(?<=invert\\=)(.*?)(?=,centerOption)"
+const offset_edit_rgx = "(?<=offset\\=)(.*?)(?=,invert)"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -25,6 +26,21 @@ func _ready():
 	wp_cen_ctrl.hint_tooltip = tr("WP_CEN_CTRL_HINT_TOOLTIP")
 	wp_cen_group.add_child(wp_cen_ctrl)
 	add_child(wp_cen_group)
+
+	# Allow the user to set the offset for the workplane away from the origin in the direction defined by the normal
+	var offset_group = HBoxContainer.new()
+	offset_group.name = "offset_group"
+	var offset_lbl = Label.new()
+	offset_lbl.set_text("Offset: ")
+	offset_group.add_child(offset_lbl)
+	var offset_ctrl = NumberEdit.new()
+	offset_ctrl.name = "offset_ctrl"
+	offset_ctrl.set_text("0.0")
+	offset_ctrl.size_flags_horizontal = 3
+	offset_ctrl.hint_tooltip = tr("WP_OFFSET_CTRL")
+	offset_ctrl.CanBeNegative = true
+	offset_group.add_child(offset_ctrl)
+	add_child(offset_group)
 
 	# Allow the user to set whether the workplane normal is inverted
 	var invert_group = HBoxContainer.new()
@@ -50,6 +66,12 @@ func is_binary():
 Checks whether or not all the values in the controls are valid.
 """
 func is_valid():
+	var offset_ctrl = get_node("offset_group/offset_ctrl")
+
+	# Make sure all of the numeric controls have valid values
+	if not offset_ctrl.is_valid:
+		return false
+
 	return true
 
 
@@ -59,12 +81,13 @@ Fills out the template and returns it.
 func get_completed_template():
 	var wp_cen_ctrl = find_node("wp_cen_ctrl", true, false)
 	var invert_ctrl = find_node("invert_ctrl", true, false)
+	var offset_ctrl = get_node("offset_group/offset_ctrl")
 
 	var complete = ""
 
-	# Use the simple template
+	# Use the template
 	complete = template.format({
-#		"comp_name": wp_name_ctrl.get_text(),
+		"offset": offset_ctrl.get_text(),
 		"center_option": wp_cen_ctrl.get_item_text(wp_cen_ctrl.get_selected_id()),
 		"invert": invert_ctrl.pressed
 	})
@@ -86,6 +109,7 @@ Loads values into the control's sub-controls based on a code string.
 func set_values_from_string(text_line):
 	var wp_cen_ctrl = find_node("wp_cen_ctrl", true, false)
 	var invert_ctrl = find_node("invert_ctrl", true, false)
+	var offset_ctrl = get_node("offset_group/offset_ctrl")
 
 	prev_template = text_line
 
@@ -96,6 +120,13 @@ func set_values_from_string(text_line):
 	var res = rgx.search(text_line)
 	if res:
 		Common.set_option_btn_by_text(wp_cen_ctrl, res.get_string())
+
+	# The offset option
+	rgx.compile(offset_edit_rgx)
+	res = rgx.search(text_line)
+	if res:
+		# Fill in the offset text
+		offset_ctrl.set_text(res.get_string())
 
 	# The invert option
 	rgx.compile(invert_edit_rgx)
